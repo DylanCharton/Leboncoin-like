be05192123b14784a50bcb3634275afb9ceb51ac
DylanCharton <d.charton@codeur.online>
2021-10-20 16:01:28 +0200

    Architecture

---
+++ class/Database.php
@@ -0,0 +1,21 @@
+<?php
+
+class Database
+{
+
+   public function connect() 
+ {
+     try
+     {
+         $bdd = new PDO('mysql:host=localhost;dbname=leboncoin;port=3306;charset=utf8', 'root', '');
+         echo "Connected";
+        return $bdd; 
+      
+     }
+     catch(Exception $e)
+     {
+         die('Erreur : '.$e->getMessage());
+     }
+ }
+    
+}
