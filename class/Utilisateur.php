3f1b1bb9aa54e5b238ea0fed988bd4e8f2a1961b
BU33R <t.laubepin@codeur.online>
2021-10-21 12:17:38 +0200

    Récuperation

--- class/Utilisateur.php
+++ class/Utilisateur.php
@@ -2,24 +2,31 @@
     require_once("Database.php");
 class Utilisateur extends Database
 {
-     
+    private $connexion;
     private $username;
     private $password;
     private $mail;
 
     public function __construct($username, $password, $mail){
-        $this->username = $username;
-        $this->password = $password;
-        $this->mail = $mail;
+
+        $this->username = strip_tags($username);
+        $this->password = strip_tags($password);
+        $this->mail = strip_tags($mail);
+
+
     }
 
+
     public function addUser($username, $password, $mail){
-        $connexion = new Database();
+    
         $newUsername = strip_tags($username);
         $newPassword = strip_tags($password);
         $newMail = strip_tags($mail);
+        
 
-        $connexion->connect();
+        $sql = $connexion->connect->prepare("INSERT INTO user(name_user, pass_user, mail_user) VALUE (:user,:pass,:mail)");
+        $sql
+        $sql->prepare("
         return $connexion;
     }
 }
