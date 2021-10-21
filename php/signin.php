db64b5e9a5423bf93569a695ec01bd5dde1167a9
BU33R <t.laubepin@codeur.online>
2021-10-21 16:43:59 +0200

    Avancement du CSS et du SIGNIN

---
+++ php/signin.php
@@ -0,0 +1,37 @@
+<?php  
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/signin.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <form class="row g-3">
+        <div class="col-sm-1">
+            <label for="inputUser" class="form-label">User</label>
+            <input type="user" class="form-control" id="inputUser">
+        </div>
+        <div class="col-sm-1">
+            <label for="inputPassword" class="form-label">Password</label>
+            <input type="password" class="form-control" id="inputPassword">
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+    </form>
+</body>
+
+</html>
