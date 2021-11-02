3740640cecb1cb50a40db059e0fbff150713a2c2
DylanCharton <d.charton@codeur.online>
2021-11-02 10:21:51 +0100

    Modificated CSS and added the user session variable

--- php/login.php
+++ php/login.php
@@ -1,17 +1,21 @@
-<html>
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <!-- importer le fichier de style -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS & BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/login.css" media="screen" type="text/css" />
+    <title>The Good Corner - Connexion</title>
 </head>
-
+    
 <body>
     <div id="container">
         <!-- zone de connexion -->
 
-        <form action="#" method="POST">
-            <h1>Connexion</h1></br>
+        <form action="#" method="POST" class="d-flex justify-content-center flex-column align-items-center">
+            <h1 class="pb-2">Connexion</h1>
 
             <label><b>Nom d'utilisateur : </b></label>
             <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
@@ -20,10 +24,11 @@
         
             <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-            <input type="submit" id='submit' value='LOGIN'>
+            <input type="submit" id='submit' value='Se connecter' class="px-3 py-2">
 
-            <div class="d-flex justify-content-center links">
-            </br> Avez-vous un compte  ?<a href="signin.php"></br></br>Inscrivez-vous !</a>
+            <div class="d-flex justify-content-center links flex-column">
+            <p class="pt-3 pb-0">Vous n'avez pas de compte ?</p>
+            <a href="signin.php">Inscrivez-vous</a>
 		    </div>
 	
         </form>     
--- php/signin.php
+++ php/signin.php
@@ -6,43 +6,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/signin.css">
+    <link rel="stylesheet" href="../css/login.css">
     <title>Inscription</title>
+    <!-- Tommy, commente ici pour expliquer à quoi servent les deux scripts que tu appelles stp -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
     <script src="../js/mdp.js"></script>
 </head>
 
 <body>
-
-    <?php  require_once('../class/Utilisateur.php');?>
     <div id="container">
 
-        <!-- zone de connexion -->
-
-        <form action="#" method="POST">
+        <form action="#" method="POST" class="d-flex flex-column align-items-center ">
             <h1>Inscription</h1>
 
-            <label><b>Nom d'utilisateur : </b></label>
+            <label class="signin-label">Nom d'utilisateur :</label>
             <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
 
-            <label><b>Mot de passe : </b></label>
-            <input type="password" placeholder="Entrer le mot de passe" name="password" id="password" required title="Votre mot de passe doit contenir au moins :
+            <label class="signin-label">Mot de passe :</label>
+            <input type="password" placeholder="Entrer le mot de passe" name="password" id="password" required title="Votre mot de passe doit contenir:
  8 caractères minimum
  1 chiffre minimum
  1 lettre minuscule minimum
  1 lettre majuscule minimum
- (50 caractères maximum)">
+ Ne doit pas dépasser 50 caractères.">
 
-            <span id="affichageMessage"></span></br>
+            <span id="affichageMessage" class="mb-2"></span>
 
 
-            </br><label><b>Email : </b></label>
+            <label class="signin-label">Email : </label>
             <input type="mail" placeholder="Entrer votre adresse mail" name="mail" required>
 
-            <input type="submit" id='submit' value="S'inscrire">
+            <input type="submit" id='submit' value="S'inscrire" class="px-3 py-2">
 
             <?php
-        
+        // Starting by requiring the Utilisateur class to call the method to add a user to the DB.
             require_once("../class/Utilisateur.php");
         
 
